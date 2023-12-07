41ff06ead1e7f2035d6462b49306840577899e08
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-12-07 10:30:18 -0600

    edited step model into schema

--- server/models/Goal.js
+++ server/models/Goal.js
@@ -1,26 +1,23 @@
 const { Schema, model } = require('mongoose');
+const stepSchema = require("./stepSchema")
 
 const goalSchema = new Schema({
   name: {
     type: String,
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: function (time) {
-      return new Date(time).toLocaleDateString()
-    }
-  },
   completed: {
     type: Boolean,
     default: false
   },
-  steps: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Step'
-  }]
-});
+  steps: [
+    stepSchema
+  ]
+},
+  {
+    timestamps: true,
+  }
+);
 
 const Goal = model('Goal', goalSchema);
 module.exports = Goal;

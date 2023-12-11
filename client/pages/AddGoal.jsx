d5403bad70540c47aa28940b0d0cb88968070f34
Joey Thao <thaojoey5@gmail.com>
2023-12-11 12:59:16 -0600

    css for addGoal page

--- client/pages/AddGoal.jsx
+++ client/pages/AddGoal.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { GoalCreate, GoalSteps } from "../components";
+import '../css/AddGoal.css'
+import stepImage from '../image/pic5.png'
 
 export default function AddGoal() {
   const [goal, setGoal] = useState("");
@@ -15,35 +17,38 @@ export default function AddGoal() {
 
   return (
     <>
-      {/* if no goal has been submitted, display goal create form */}
-      {!goalSelected ? (
-        <>
-          <GoalCreate
-            goal={goal}
-            setGoal={setGoal}
-            setGoalSelected={setGoalSelected}
-            setSteps={setSteps}
-          ></GoalCreate>
-        </>
-      ) :
-        // if goal has been submitted, render this
-        <>
-          {/* makes sure steps exist before rendering */}
-          {steps ? (
-            <GoalSteps
-              steps={steps}
-              setSteps={setSteps}
-              reset={reset}
+      <img src={stepImage} className="stepImage" alt="stepImage" />
+      <body className="body">
+        {/* if no goal has been submitted, display goal create form */}
+        {!goalSelected ? (
+          <>
+            <GoalCreate
               goal={goal}
               setGoal={setGoal}
-            ></GoalSteps>
-          ) : (
-            // while waiting on API response, display this
-            // REPLACE THIS WITH LEAH'S LOADING ANIMATION
-            <div className="text-success">Loading...</div>
-          )}
-        </>
-      }
+              setGoalSelected={setGoalSelected}
+              setSteps={setSteps}
+            ></GoalCreate>
+          </>
+        ) :
+          // if goal has been submitted, render this
+          <>
+            {/* makes sure steps exist before rendering */}
+            {steps ? (
+              <GoalSteps
+                steps={steps}
+                setSteps={setSteps}
+                reset={reset}
+                goal={goal}
+                setGoal={setGoal}
+              ></GoalSteps>
+            ) : (
+              // while waiting on API response, display this
+              // REPLACE THIS WITH LEAH'S LOADING ANIMATION
+              <div className="text-success">Loading...</div>
+            )}
+          </>
+        }
+      </body>
     </>
   )
 }

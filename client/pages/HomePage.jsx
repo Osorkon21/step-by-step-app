aca7139e6fa22ed068c0ecab82910853df441694
Joey Thao <thaojoey5@gmail.com>
2023-12-10 21:36:36 -0600

    work on css for homepage

--- client/pages/HomePage.jsx
+++ client/pages/HomePage.jsx
@@ -1,13 +1,12 @@
-// import './HomePage.css'
+import './HomePage.css'
 
 export default function HomePage() {
 
   return (
     <>
-      <h1>About This App</h1>
+      <h1 className='about app'>About This App</h1>
       <p>Want to accomplish things but don't know how to begin? Start here! Sign up to start creating step-by-step paths to accomplishing your goals (terrible, change this). All-knowing AI will provide you with necessary steps to check your tasks off your to-do list.</p>
-
-      <h2>Walkthrough .gif/video goes here</h2>
+      <h2 className='video'>Walkthrough .gif/video goes here</h2>
     </>
   )
 }

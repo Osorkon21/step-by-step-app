4c25460af3282934da9f62c34cf73ce486a9d615
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-12-11 22:28:09 -0600

    removed faulty import

--- client/components/GoalCreate.jsx
+++ client/components/GoalCreate.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { v4 as uuidv4 } from "uuid"
-import getRandomNumber from '../utils/randomizer'
+
 
 export default function GoalCreate({ goal, setGoal, setGoalSelected, setSteps }) {
   const [submitError, setSubmitError] = useState("");

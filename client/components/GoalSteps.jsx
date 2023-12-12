b6539de5b44ff02c0e762b4fbde93a0261242d71
Joey Thao <thaojoey5@gmail.com>
2023-12-12 14:16:51 -0600

    test

--- client/components/GoalSteps.jsx
+++ client/components/GoalSteps.jsx
@@ -141,12 +141,10 @@ export default function GoalSteps({ steps, setSteps, reset, goal, setGoal, usage
       <form onSubmit={handleFormSubmit} className="form">
         <div className="d-flex align-items-center">
           <label htmlFor="goal" className="goalTitle">Goal Title:</label>
-          {/* <label htmlFor="complete-all" className="markComplete">Mark All Completed:</label> */}
+          <label htmlFor="complete-all" className="markComplete">Mark All Completed:</label>
           <input className="checkbox" type="checkbox" defaultChecked={false} id="complete-all" onChange={handleCheck} />
           <div className="w-50">
-            <label htmlFor="complete-all" className="markComplete">Mark All Completed:</label>
-            {/* <label htmlFor="goal" className="goalTitle">Goal Title:</label> */}
-            <input type="text" name="goal" id="goal" className="form-control" value={goal.name} onChange={handleInputChange} />
+            <input type="text" name="goal" id="goal" className="form-control1" value={goal.name} onChange={handleInputChange} />
           </div>
           <button className="ms-2" type="reset" onClick={reset}>Clear All</button>
         </div>

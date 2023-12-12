5059bb522f7b0110606c912706f3f24cb0289ac6
Joey Thao <thaojoey5@gmail.com>
2023-12-12 00:31:39 -0600

    css for AuthPage

--- client/pages/AuthPage.jsx
+++ client/pages/AuthPage.jsx
@@ -1,18 +1,25 @@
 import { useState, useEffect } from "react"
 import Auth from "../components/Auth"
+import join from '../image/pic8.png'
+import freeMember from '../image/pic9.png'
+import '../css/AuthPage.css'
 
 export default function AuthPage(){
 
 
   return (
-    <div className="d-flex gap-5">
-      <div>
-        <Auth usage="signup" />
-      </div>
+    <>
+        <div className="d-flex gap-5">
+          <div>
+            <Auth usage="signup" />
+          </div>
 
-      <div>
-        <Auth usage="login" />
-      </div>
-    </div>
+          <div>
+            <Auth usage="login" />
+          </div>
+        </div>
+      <img src={join} className="join" alt="join" />
+      <img src={freeMember} className="freeMember" alt="freeMember" />
+    </>
   )
 }

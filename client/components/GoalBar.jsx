6fbdcf902b174e7e44baabf630d06f2d07766ff5
Osorkon21 <Osorkon21@users.noreply.github.com>
2023-12-13 18:45:07 -0600

    starting to add collapse on click

--- client/components/GoalBar.jsx
+++ client/components/GoalBar.jsx
@@ -2,12 +2,17 @@ import trashCan from "../assets/icons/trash-can.svg"
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export default function GoalBar({ goal, setCurrentGoal, deleteGoal }) {
+
+  function handleBarClick(goal) {
+
+  }
+
   const now = Math.floor(goal.completedStepCount / goal.stepsCount * 100);
 
   return (
     <>
       <div className="article-container mx-4 d-flex">
-        <button className="btn btn-secondary mt-3 w-100 d-flex justify-content-between align-items-center" type="button" onClick={() => setCurrentGoal(goal)}>
+        <button className="btn btn-secondary mt-3 w-100 d-flex justify-content-between align-items-center" type="button" onClick={() => handleBarClick(goal)}>
           <span>{goal.name}</span>
           <ProgressBar striped variant="success" className="w-50" now={now} label={now ? `${now}%` : ""} />
           <span>{new Date(goal.createdAt).toLocaleDateString()}</span>

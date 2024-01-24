c2574e88c381e1ef95f39bc89697d3e48787ddee
Osorkon21 <Osorkon21@users.noreply.github.com>
2024-01-24 16:41:07 -0600

    Add signup switch to login modal, add Hello Username component

--- client/components/SignupModal.jsx
+++ client/components/SignupModal.jsx
@@ -1,17 +1,36 @@
 import { Button, Dialog, DialogTrigger, Modal } from 'react-aria-components';
 import { Auth } from "./"
+import { useState } from "react";
 
-export default function SignupModal({ usage = "login", buttonText }) {
+export default function SignupModal({ buttonText }) {
+  const [usage, setUsage] = useState("login");
+
+  const signupStyle = {
+    textDecoration: "underline blue",
+    color: "blue",
+    cursor: "pointer"
+  };
+
+  function flipUsage() {
+    if (usage === "login")
+      setUsage("signup");
+    else if (usage === "signup")
+      setUsage("login");
+  }
+
   return (
     <DialogTrigger>
       <Button>{buttonText}</Button>
       <Modal isDismissable>
         <Dialog>
           {({ close }) => (
-            <Auth
-              usage={usage}
-              close={close}
-            ></Auth>
+            <>
+              <Auth
+                usage={usage}
+                close={close}
+              ></Auth>
+              <p>{usage === "login" ? "No account? " : "Back to "}<span style={signupStyle} onClick={() => flipUsage()}>{usage === "login" ? "Sign up!" : "Login"}</span></p>
+            </>
           )}
         </Dialog>
       </Modal>

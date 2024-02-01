f20579fd8c8658b25b87f8c5e4e57d986598cc85
Osorkon21 <Osorkon21@users.noreply.github.com>
2024-02-01 16:38:20 -0600

    Debug StepBar

--- client/src/components/StepBarClosed.jsx
+++ client/src/components/StepBarClosed.jsx
@@ -1,6 +1,6 @@
 export default function StepBarClosed({ step, handleStepBarClick }) {
   return (
-    <div className="mx-4 border-2 border-red-600" onClick={handleStepBarClick}>
+    <div className="mx-4 border-2 border-red-600 cursor-pointer" onClick={handleStepBarClick}>
       <div className="mt-3 items-center">
         {step.title || "Select to edit step"}
       </div>

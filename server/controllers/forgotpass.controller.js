31c0fc1dbb89b3e642d6815e68c670afe49bb952
Leah Tillman <leahtillman@leahs-air.lan>
2024-02-15 16:53:28 -0600

    added comments in controller files in prep for AWS to be ready

--- server/controllers/forgotpass.controller.js
+++ server/controllers/forgotpass.controller.js
@@ -1,13 +1,23 @@
 const { User } = require('../models');
 // const Model = User;
 const nodemailer = require('nodemailer');
-const { google } = require('google-auth-library');
+// const { google } = require('google-auth-library');
 
-const client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
-);
+// const client = new google.auth.OAuth2(
+//     process.env.CLIENT_ID,
+//     process.env.CLIENT_SECRET,
+//     process.env.REDIRECT_URI
+// );
+
+// const transporter = nodemailer.createTransport({
+//     host: 'your-smtp-server.com', // Replace with \ SMTP server
+//     port: 587, // Replace with SMTP server port
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//         user: 'your-email@example.com', //USE ENV
+//         pass: 'your-email-password', 
+//     },
+// });
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -21,14 +31,14 @@ exports.forgotPassword = async (req, res) => {
     const { email } = req.body;
 
     try {
-        // Check if the user with the given email exists
+        // Check if the user exists
         const user = await User.findOne({ email });
 
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        // Generate a reset token and save it to the user in the database
+        // Generate a reset token and save it to the user
         const resetToken = user.generateResetToken();
         await user.save();
 
@@ -38,7 +48,7 @@ exports.forgotPassword = async (req, res) => {
             from: process.env.EMAIL_USERNAME,
             to: email,
             subject: 'If this fails... I blame Kurt!',
-            html: `Click <a href="${resetLink}">here</a> to reset your password, you idiot.`,
+            html: `Click <a href="${resetLink}">here</a> to reset your password, you dummy.`,
         };
 
         await transporter.sendMail(mailOptions);

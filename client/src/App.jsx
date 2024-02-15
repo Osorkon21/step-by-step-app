d15ca99fbae9ec3487778ac9e1c8cc9ebd7437c1
Osorkon21 <Osorkon21@users.noreply.github.com>
2024-02-15 16:52:43 -0600

    Add helloUser to header, changed some CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppProvider from './utils/AppProvider';
-import { Header, HelloUser, ProtectedRoute } from './components';
+import { Header, ProtectedRoute } from './components';
 import { HomePage, Dashboard, AddGoal, NotFound } from './pages/'
 
 export default function App() {
@@ -8,28 +8,23 @@ export default function App() {
   return (
     <AppProvider>
       <BrowserRouter>
-        <>
-          <div className="">
-
-            <div className='header-container'>
-              <Header className="header" />
-            </div>
-            <HelloUser />
-
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/dashboard" element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              } />
-              <Route path="/addgoal" element={<AddGoal />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-            <footer className="footer flex justify-center p-4 text-center">&copy; 2023-2024 SBS Development Group</footer>
+        <div className="">
+          <div className='header-container'>
+            <Header className="header" />
           </div>
 
-        </>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } />
+            <Route path="/addgoal" element={<AddGoal />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <footer className="footer flex justify-center p-4 text-center">&copy; 2023-2024 SBS Development Group</footer>
+        </div>
       </BrowserRouter>
     </AppProvider>
   )

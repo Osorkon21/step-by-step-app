28fba14f1c95a5e74a9864c93fc654357744ac9a
KWensmann <kurtwensmann@gmail.com>
2024-03-20 16:56:11 -0500

    added a addgoal button to the dashboard

--- client/src/pages/NewGoal.jsx
+++ client/src/pages/NewGoal.jsx
@@ -3,7 +3,7 @@ import AddGoal from "../components/AddGoal";
 export default function Newgoal() {
   return (
     <div className='body mt-24 text-purple'>
-      <h1>New Goal</h1>  
+      <h1 className="text-5xl md:text-6xl my-8">New Goal</h1>  
       <AddGoal className="mt-1" />
     </div>
   )

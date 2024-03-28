189d79d2621afc332c988404cbe084a370fce527
Osorkon21 <Osorkon21@users.noreply.github.com>
2024-03-28 15:23:31 -0500

    Add Dashboard autosave on step expand/collapse

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -45,6 +45,62 @@ export default function Dashboard() {
     setCurrentGoal({ ...currentGoal, steps: steps });
   }
 
+  async function updateCurrentGoal() {
+    // remove all steps without a title
+    const filteredSteps = currentGoal.steps.filter(step => step.title);
+
+    // do not update database if required data is missing
+    if (!currentGoal.name || !filteredSteps.length) {
+      console.log("currentGoal was missing a required field, did not update database")
+      return;
+    }
+
+    const catToUse = categories.find((cat) => cat.name === currentGoal.category.name);
+
+    const newGoal = {
+      name: currentGoal.name,
+
+      // if all steps are completed, goal is completed
+      completed: filteredSteps.every((step) => step.completed),
+
+      category: catToUse.id,
+      steps: filteredSteps
+    }
+
+    try {
+      const query = await fetch(`/api/goals/${currentGoal._id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ goal: newGoal }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      const response = await query.json();
+
+      if (response.result === "success") {
+        const updatedGoal = response.payload;
+
+        const newGoals = goals.map((goal) => {
+          if (goal._id === updatedGoal._id)
+            return {
+              ...updatedGoal,
+              category: catToUse
+            };
+
+          return goal;
+        })
+
+        setGoals(newGoals);
+        renderGoals(newGoals);
+      }
+      else {
+        console.log("Database error - unable to save goal!", response);
+      }
+    }
+    catch (err) {
+      console.log(err.message)
+    }
+  }
+
   async function getCategories() {
     try {
       // get all categories
@@ -156,13 +212,10 @@ export default function Dashboard() {
           {inProgressGoals.map(goal => (
             <div className="goal-container" key={goal._id}>
               <GoalBar
-                goals={goals}
-                setGoals={setGoals}
-                renderGoals={renderGoals}
                 goal={goal}
-                categories={categories}
                 currentGoal={currentGoal}
                 setCurrentGoal={setCurrentGoal}
+                updateCurrentGoal={updateCurrentGoal}
                 deleteGoal={deleteGoal}
                 setSubmitError={setSubmitError}
               ></GoalBar>
@@ -175,6 +228,7 @@ export default function Dashboard() {
                     reset={reset}
                     goal={currentGoal}
                     setGoal={setCurrentGoal}
+                    updateCurrentGoal={updateCurrentGoal}
                     setSubmitError={setSubmitError}
                     usage="updateGoal"
                     defaultChecked={false}
@@ -200,13 +254,10 @@ export default function Dashboard() {
           {completedGoals.map(goal => (
             <div className="goal-container" key={goal._id}>
               <GoalBar
-                goals={goals}
-                setGoals={setGoals}
-                renderGoals={renderGoals}
                 goal={goal}
-                categories={categories}
                 currentGoal={currentGoal}
                 setCurrentGoal={setCurrentGoal}
+                updateCurrentGoal={updateCurrentGoal}
                 deleteGoal={deleteGoal}
                 setSubmitError={setSubmitError}
               ></GoalBar>
@@ -219,6 +270,7 @@ export default function Dashboard() {
                     reset={reset}
                     goal={currentGoal}
                     setGoal={setCurrentGoal}
+                    updateCurrentGoal={updateCurrentGoal}
                     setSubmitError={setSubmitError}
                     usage="updateGoal"
                     defaultChecked={true}

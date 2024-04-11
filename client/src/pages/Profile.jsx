c8b61c261029228b7ad2a256bc6dbd434bcd6259
KWensmann <kurtwensmann@gmail.com>
2024-04-11 16:46:00 -0500

    adjusted profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -134,67 +134,73 @@ export default function Profile() {
   }, [newPwd, confirmPwd]);
 
   return (
-    <div className="profile body mt-24 text-purple text-center">
+    <div className="profile body text-purple text-center bg-middleblur h-screen">
       <h1 className="text-5xl md:text-6xl my-8">User Profile</h1>
 
       {appCtx.user?._id !== undefined && (
-        <form className="flex flex-col gap-2 items-center justify-center" onSubmit={handleSubmit}>
-          <div className="gap-2 flex flex-col">
-            <label htmlFor="username">Username</label>
-            <input type="text" placeholder="What's your new username?" name="username" onChange={handleInput} value={userData.username || ""} />
+        <form className="grid sm:grid-cols-2 gap-8  justify-center" onSubmit={handleSubmit}>
+          <div className="column-1">
+            <div className="gap-2 flex flex-col">
+              <label htmlFor="username">Username</label>
+              <input type="text" placeholder="What's your new username?" name="username" onChange={handleInput} value={userData.username || ""} />
+            </div>
+
+            <div className="gap-2 flex flex-col">
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" placeholder="What's your first name?" name="firstName" onChange={handleInput} value={userData.firstName || ""} />
+            </div>
+
+            <div className="gap-2 flex flex-col">
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" placeholder="What's your last name?" name="lastName" onChange={handleInput} value={userData.lastName || ""} />
+            </div>
+
+            <div className="gap-2 flex flex-col">
+              <label htmlFor="userBio">Bio</label>
+              <textarea type="text" placeholder="What's your story?" name="userBio" rows={4} onChange={handleInput} value={userData.userBio || ""} />
+            </div>
+
+          </div>
+          <div className="column-2">
+
+            <div className="gap-2 flex flex-col">
+              <label htmlFor="pronouns">Pronouns</label>
+
+              <Select className="" placeholder={userData.pronouns || "Choose Pronouns"} aria-label="pronoun select dropdown" onSelectionChange={handleSelect}>
+                <Button className="p-2 border-2 border-purple bg-middle">
+                  <SelectValue />
+                  <span aria-hidden="true">▼</span>
+                </Button>
+                <Popover className="bg-middle p-4 rounded-lg hover:bg-purple hover:text-white focus:outline-none">
+                  <ListBox className="">
+                    {pronouns.map((pronoun) => (
+                      <ListBoxItem className="hover:bg-purple hover:text-white rounded-md p-1" key={pronoun} id={pronoun}>{pronoun}</ListBoxItem>
+                    ))}
+                  </ListBox>
+                </Popover>
+              </Select>
+            </div>
+            <div className="gap-2 flex flex-col">
+              <label htmlFor="email">Current Email</label>
+              <input type="email" name="email" disabled value={appCtx.user.email || ""} />
+            </div>
+
+            <div className="gap-2 flex flex-col">
+              <label htmlFor="newEmail">New Email</label>
+              <input type="email" placeholder="What is your new email?" name="newEmail" onChange={(e) => setNewEmail(e.target.value)} value={newEmail || ""} />
+
+            <div className="gap-2 flex flex-col">
+              <label htmlFor="confirmEmail">Confirm New Email</label>
+              <input type="email" placeholder="Confirm new email" name="confirmEmail" onChange={(e) => setConfirmEmail(e.target.value)} value={confirmEmail || ""} />
+            </div>
+
+            <div className="text-red-600">
+              {emailError}
+            </div>
+            </div>
           </div>
 
-          <div className="gap-2 flex flex-col">
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" placeholder="What's your first name?" name="firstName" onChange={handleInput} value={userData.firstName || ""} />
-          </div>
 
-          <div className="gap-2 flex flex-col">
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" placeholder="What's your last name?" name="lastName" onChange={handleInput} value={userData.lastName || ""} />
-          </div>
-
-          <div className="gap-2 flex flex-col">
-            <label htmlFor="userBio">Bio</label>
-            <textarea type="text" placeholder="What's your story?" name="userBio" rows={4} onChange={handleInput} value={userData.userBio || ""} />
-          </div>
-
-          <div className="gap-2 flex flex-col">
-            <label htmlFor="pronouns">Pronouns</label>
-
-            <Select className="" placeholder={userData.pronouns || "Choose Pronouns"} aria-label="pronoun select dropdown" onSelectionChange={handleSelect}>
-              <Button className="p-2 border-2 border-purple hover:scale-95">
-                <SelectValue />
-                <span aria-hidden="true">▼</span>
-              </Button>
-              <Popover className="bg-middle p-4 rounded-lg hover:bg-purple hover:text-white focus:outline-none">
-                <ListBox className="">
-                  {pronouns.map((pronoun) => (
-                    <ListBoxItem className="hover:bg-purple hover:text-white rounded-md p-1" key={pronoun} id={pronoun}>{pronoun}</ListBoxItem>
-                  ))}
-                </ListBox>
-              </Popover>
-            </Select>
-          </div>
-
-          <div className="gap-2 flex flex-col">
-            <label htmlFor="email">Current Email</label>
-            <input type="email" name="email" disabled value={appCtx.user.email || ""} />
-          </div>
-
-          <div className="gap-2 flex flex-col">
-            <label htmlFor="newEmail">New Email</label>
-            <input type="email" placeholder="What is your new email?" name="newEmail" onChange={(e) => setNewEmail(e.target.value)} value={newEmail || ""} />
-          </div>
-
-          <div className="gap-2 flex flex-col">
-            <label htmlFor="confirmEmail">Confirm New Email</label>
-            <input type="email" placeholder="Confirm new email" name="confirmEmail" onChange={(e) => setConfirmEmail(e.target.value)} value={confirmEmail || ""} />
-          </div>
-
-          <div className="text-red-600">
-            {emailError}
-          </div>
 
           <div className="gap-2 flex flex-col">
             <label htmlFor="newPassword">New Password</label>
@@ -210,7 +216,7 @@ export default function Profile() {
             {pwdError}
           </div>
 
-          <button className="update-goal-btn hover:scale-95" type="submit">Save Changes</button>
+          <button className="update-goal-btn " type="submit">Save Changes</button>
 
           <div className="changesSaved"></div>
 

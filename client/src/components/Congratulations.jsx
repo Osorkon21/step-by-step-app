29c82eb8c0557fa69abc28cc86cee72a938d1600
Osorkon21 <Osorkon21@users.noreply.github.com>
2024-04-18 16:43:28 -0500

    Add celebration on goal completion

---
+++ client/src/components/Congratulations.jsx
@@ -0,0 +1,7 @@
+export default function Congratulations() {
+  return (
+    <div className="fixed p-12 bg-white bg-opacity-75 rounded-full text-6xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      GOAL COMPLETED!
+    </div>
+  )
+}
